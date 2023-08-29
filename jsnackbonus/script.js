f9e04f8debe01ece71a0c4aa07fcad961adec54a
PietroCipolla95 <pietro.cipolla.95@gmail.com>
2023-08-29 15:55:30 +0200

    push

---
+++ jsnackbonus/script.js
@@ -0,0 +1,22 @@
+/* 
+
+Crea un array vuoto.
+Chiedi per 6 volte all’utente di inserire un numero,
+se è dispari inseriscilo nell’array.
+
+*/
+
+
+const oddList = []
+
+let typeNumberOne = Number(prompt('type a number'));
+let typeNumberTwo = Number(prompt('type a number'));
+let typeNumberThree = Number(prompt('type a number'));
+let typeNumberFour = Number(prompt('type a number'));
+let typeNumberFive = Number(prompt('type a number'));
+let typeNumberSix = Number(prompt('type a number'));
+
+
+
+
+
