54009ae3c02658942f7423766f398656b4a5e655
PietroCipolla95 <pietro.cipolla.95@gmail.com>
2023-08-29 15:20:46 +0200

    initial commit

---
+++ jsnack1/script.js
@@ -0,0 +1,11 @@
+/* 
+
+L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore.
+
+*/
+
+const numberOne = prompt('type a number')
+const numberTwo = prompt('write another number'
+
+)
