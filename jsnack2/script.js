c86269cf138aabaee2ab274bd0eb7940b175e3d1
PietroCipolla95 <pietro.cipolla.95@gmail.com>
2023-08-30 11:56:43 +0200

    add commas

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -5,14 +5,14 @@ Il software stampa prima la parola più corta, poi la parola più lunga.
 
 */
 
-/* const firstWord = prompt('type a word')
+const firstWord = prompt('type a word')
 const secondWord = prompt('type another word')
 
 if (firstWord.length > secondWord.length) {
     document.getElementById('print').innerHTML = firstWord + '  ' + secondWord;
 } else if (firstWord.length < secondWord.length) {
     document.getElementById('print').innerHTML = secondWord + '  ' + firstWord;
-} */
+}
 
 
 /* 
@@ -34,17 +34,17 @@ while (i < 2) {
 
     console.log(userWord);
 
-    array.push(userWord)
+    array.push(userWord);
     
 }
 
 if (array[0].length > array[1].length) {
 
-    document.getElementById('print').innerHTML = (array[1] + '  ' + array[0])
+    document.getElementById('print').innerHTML = (array[1] + '  ' + array[0]);
 
 } else if (array[0].length < array[1].length) {
 
-    document.getElementById('print').innerHTML = (array[0] + '  ' + array[1])
+    document.getElementById('print').innerHTML = (array[0] + '  ' + array[1]);
 
 } 
 
